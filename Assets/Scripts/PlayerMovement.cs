82301a8bf2449d7691b4f70c2e0c3d02e6ad4f29
FortKnox17 <knox.mcgraw10@gmail.com>
2026-06-15 17:04:39 -0400

    fixed player jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,11 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float movespeed = 5f;
     public float jumpforce = 10f;
+    private int jumpCount = 1;
+    private int maxJumpCount = 1;
+    private float jumpResetCoolDown = 1f;
+    public GameObject groundCheck;
+    public LayerMask ground;
     float horizontal;
     float vertical;
     public Rigidbody2D rb;
@@ -20,6 +25,11 @@ public class PlayerMovement : MonoBehaviour
     {
         
         rb.linearVelocity = new Vector2(horizontal * movespeed, rb.linearVelocity.y);
+        if (Physics2D.OverlapArea(new Vector2(groundCheck.transform.position.x-0.55f,groundCheck.transform.position.y + 0.5f),new Vector2(groundCheck.transform.position.x + 0.55f, groundCheck.transform.position.y - 0.1f), ground) && jumpResetCoolDown>5)
+        {
+            jumpCount = maxJumpCount;
+        }
+        jumpResetCoolDown+= Time.deltaTime;
     }
     
     public void Move(InputAction.CallbackContext context)
@@ -32,7 +42,13 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.performed)
         {
-            rb.AddForce(new Vector2(0f, jumpforce), ForceMode2D.Impulse);
+            if (jumpCount > 0) {
+                rb.AddForce(new Vector2(0f, jumpforce), ForceMode2D.Impulse);
+                jumpCount -= 1;
+                jumpResetCoolDown = 0;
+            }
+            
         }
     }
+
 }
