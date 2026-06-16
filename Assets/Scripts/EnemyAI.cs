30658a9690453e5d7ef1f3b21ce961a98cbae09c
tprai <jamie.l.kindt@gmail.com>
2026-06-16 10:33:53 -0400

    Update EnemyAI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public static int enemyCount = 0;
+    private int myID;
+    public Rigidbody2D me;
+    public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        me = GetComponent<Rigidbody2D>();
+        myID = enemyCount;
+        enemyCount++;
+        Debug.Log("Loaded Enemy #" + myID);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocityY = 1;
     }
 }
