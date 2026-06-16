5087d3fb56a4974a3060b5690e300cb830c9ed64
tprai <jamie.l.kindt@gmail.com>
2026-06-16 13:06:15 -0400

    Update EnemyAI (Jump if stuck; implement dt)

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -17,18 +17,17 @@ public class EnemyAI : MonoBehaviour
         me = GetComponent<Rigidbody2D>();
         myID = enemyCount;
         enemyCount++;
-        Debug.Log("Loaded Enemy #" + myID);
     }
 
     // Update is called once per frame
     void Update()
     {
         float xToPlayer = player.transform.position.x-me.transform.position.x;
-        me.linearVelocityX += Mathf.Sign(xToPlayer);
-        if (isGrounded&&Mathf.Abs(xToPlayer)<5) Jump();
-        //friction
-        me.linearVelocityX *= 0.8f;
-
+        me.linearVelocityX += Mathf.Sign(xToPlayer)*60f*Time.deltaTime;//move towards player
+        me.linearVelocityX *= Mathf.Pow(0.8f,Time.deltaTime*60f);//friction
+        //jump if close or random
+        if (isGrounded&&(Mathf.Abs(xToPlayer)<5||Random.value<0.15f*Time.deltaTime||Mathf.Abs(xToPlayer)>0.5f&&Mathf.Abs(me.linearVelocityX) < 0.1f)) Jump();
+        
         if (Physics2D.OverlapArea(new Vector2(me.transform.position.x-0.5f,me.transform.position.y + 0.5f),new Vector2(me.transform.position.x + 0.5f, me.transform.position.y - 0.5f), ground))
              {isGrounded = true;}
         else {isGrounded = false;}
@@ -39,5 +38,4 @@ public class EnemyAI : MonoBehaviour
         me.linearVelocityY = jumpforce;
         isGrounded = false;
     }
-
 }
