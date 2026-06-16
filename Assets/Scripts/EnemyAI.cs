d20c3547a3d90dbfd6722ea856f30efec181c5b2
tprai <jamie.l.kindt@gmail.com>
2026-06-16 11:32:26 -0400

    Update EnemyAI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -25,11 +25,11 @@ public class EnemyAI : MonoBehaviour
     {
         float xToPlayer = player.transform.position.x-me.transform.position.x;
         me.linearVelocityX += Mathf.Sign(xToPlayer);
-        if (isGrounded&&xToPlayer<100) Jump();
+        if (isGrounded&&Mathf.Abs(xToPlayer)<100) Jump();
         //friction
-        me.linearVelocityX *= 0.6f;
+        me.linearVelocityX *= 0.8f;
 
-        if (Physics2D.OverlapArea(new Vector2(me.transform.position.x-0.45f,me.transform.position.y + 0.1f),new Vector2(me.transform.position.x + 0.45f, me.transform.position.y - 0.1f), ground))
+        if (Physics2D.OverlapArea(new Vector2(me.transform.position.x-0.5f,me.transform.position.y + 0.5f),new Vector2(me.transform.position.x + 0.5f, me.transform.position.y - 0.5f), ground))
              {isGrounded = true;}
         else {isGrounded = false;}
     }
