32473dbad161a9164a23aa05b7af6c2813b75487
Ethan Ma <ethanmaminecraft@gmail.com>
2026-06-16 18:47:35 -0400

    Added EnemyHP

---
+++ Assets/Scripts/EnemyHP.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SterializeField] private float maxHealth = 3f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
